56c708eaabd6e397712c794f2b55660e18d3887c
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-02-23 07:14:59 +0500

    Study asynchronous and blocking

--- src/main/java/sec01/Lec06SupplierRefactoring.java
+++ src/main/java/sec01/Lec06SupplierRefactoring.java
@@ -2,13 +2,23 @@ package sec01;
 
 import course_util.Util;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 public class Lec06SupplierRefactoring {
 
     public static void main(String[] args) {
         getName();
-        getName().subscribe(Util.onNext());
-        getName();
+
+        getName()
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe(Util.onNext());
+
+        var fullName = getName()
+                .subscribeOn(Schedulers.boundedElastic())
+                .block();
+        System.out.println(fullName);
+
+        Util.sleepSeconds(4);
     }
 
     private static Mono<String> getName() {
