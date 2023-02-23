76ff7a152ea8dca49dead8edb02e066b0f7d229b
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-02-23 07:34:40 +0500

    Study create mono from future and runnable

---
+++ src/main/java/sec01/Lec07MonoFromFuture.java
@@ -0,0 +1,24 @@
+package sec01;
+
+import course_util.Util;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Lec07MonoFromFuture {
+
+    public static void main(String[] args) {
+
+        Mono.fromFuture(getName())
+                .subscribe(
+                        Util.onNext()
+                );
+
+        Util.sleepSeconds(1);
+
+    }
+
+    private static CompletableFuture<String> getName() {
+        return CompletableFuture.supplyAsync(() -> Util.faker().name().fullName());
+    }
+}
