27c29e7078d37f7efa31a319a84f9992bd1132bc
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-02-24 14:45:19 +0500

    Investigate operations with  flux

---
+++ src/main/java/sec02/assignment/StockPricePublisher.java
@@ -0,0 +1,19 @@
+package sec02.assignment;
+
+import course_util.Util;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class StockPricePublisher {
+
+    public static Flux<Integer> getPrice() {
+        AtomicInteger atomicInteger = new AtomicInteger(100);
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(i -> atomicInteger.getAndAccumulate(
+                        Util.faker().random().nextInt(-5, 5),
+                        Integer::sum
+                ));
+    }
+}
